2d9654b01d94c9d632819c7f5e000c4487d6373a
Fábio Filho <fabio.filhossi@gmail.com>
2022-07-20 19:34:04 -0300

    chore: change default port

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/off', function (req, res) {
    res.end(JSON.stringify({"action": "off"}))
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(3007, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
